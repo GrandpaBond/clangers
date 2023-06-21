bf9e588d2fddf0827119ef7fb1f6c73ae5b6f898
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-06-21 17:44:07 +0100

    First working version

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
-enum VoxType {
+/* UTTERANCES
+While the built-in facilities for creating sound-effects are impressively flexible,
+they are insufficient to properly express moods and emotions.
+For the Emote extension, we would like to create more complex sounds, that have 
+inflections in both pitch and volume.  We call these sound-patterns "utterances".
+Examples include a laugh, a moan or an "Uh-oh" that indicates a problem.
+These can involve up to three separate sound parts that will then be played consecutively
+to give a smoothly varying result when spliced together.
+
+Internally, sound effects are encoded as a string of 72 digits, broken into several fields
+(many of which appear to be obsolete or redundant). 
+The function createSoundEffect() takes eight arguments and works hard to encode them into their
+respective fields.
+
+We would like to be able to dynamically vary the overall pitch or volume of any given utterance.
+Conventionally, that would require reconstructing all three sound effects from scratch for each 
+performance (or saving a wide range of 3*72-character strings we prepared earlier).
+Instead we choose to selectively overwrite certain 4-digit fields within our three-part 
+utterance to "tune" its pitch and volume as we require.
+
+*** It is acknowledged that this is a dangerous practice that relies on the internal
+*** representation not changing, but it is believed that the performance gains justify it!
+
+*/
+enum UtteranceType {
     NONE = 0,
     TWEET = 1,
     LAUGH = 2,
@@ -12,7 +36,9 @@ enum VoxType {
     GROWL = 10
 }
 
-// code this defensively, in case SoundExpression field-locations change in future
+
+// Code this defensively, just in case SoundExpression field-locations should change in future.
+// (We presume their values will always be 0000 to 9999)
 const startVolPos = 1
 const startFreqPos = 5
 const durationPos = 9
@@ -20,11 +46,15 @@ const endVolPos = 26
 const endFreqPos = 18
 
 class Utterance {
-    //properties
-    type: VoxType;
+    // properties
+    mytype: UtteranceType; 
     partA: soundExpression.Sound;
     partB: soundExpression.Sound;
     partC: soundExpression.Sound;
+    // Each part has a start and an end [frequency,volume], but endA===startB 
+    // and endB===startC, so an utterance moves through four [frequency,volume] points
+    // These are set up to be fixed ratios of the performance [frequency,volume]
+    // 
     freqRatio0: number;
     freqRatio1: number;
     freqRatio2: number;
@@ -33,6 +63,7 @@ class Utterance {
     volRatio1: number;
     volRatio2: number;
     volRatio3: number;
+    // Proportions of time allocated to each part
     timeRatio1: number;
     timeRatio2: number;
     timeRatio3: number;
@@ -40,8 +71,8 @@ class Utterance {
     usingC: boolean;
 
 
-    // initally base all three parts on freq=333Hz, vol=222, ms = 999 (333 each)
-    constructor(vox: VoxType, freq0: number, vol0: number,
+    // initially base all sounds arbitrarily on freq=333Hz, vol=666, ms=999 (333 each)
+    constructor(vox: UtteranceType, freq0: number, vol0: number,
         wave: WaveShape, fx: SoundExpressionEffect, shape: InterpolationCurve, freq1: number, vol1: number, ms1: number) {
         this.freqRatio0 = freq0;
         this.volRatio0 = vol0;
@@ -49,34 +80,37 @@ class Utterance {
         this.volRatio1 = vol1;
         this.timeRatio1 = ms1;
         this.partA = new soundExpression.Sound;
-        this.partA.src = music.createSoundEffect(wave, 333, 333, 222, 222, 999, fx, shape)
+        this.partA.src = music.createSoundEffect(wave, 333, 333, 666, 666, 999, fx, shape);
         // until found otherwise...
         this.usingB = false;
         this.usingC = false;
     }
-    // (add optional part B)
+ 
+    // methods...  
+    
+    // (adds an optional part B)
     usePartB(wave: WaveShape, fx: SoundExpressionEffect, shape: InterpolationCurve, freq2: number, vol2: number, ms2: number) {
         // we have a PartB...
         this.freqRatio2 = freq2;
         this.volRatio2 = vol2;
         this.timeRatio2 = ms2;
-        this.partB.src = music.createSoundEffect(wave, 333, 333, 222, 222, 999, fx, shape)
-        this.usingC = true
+        this.partB = new soundExpression.Sound;
+        this.partB.src = music.createSoundEffect(wave, 333, 333, 666, 666, 999, fx, shape);
+        this.usingB = true
     }
-    // (add optional part C)
+    // (adds an optional part C)
     usePartC(wave: WaveShape, fx: SoundExpressionEffect, shape: InterpolationCurve, freq3: number, vol3: number, ms3: number) {
         // we have a PartC as well...
         this.freqRatio3 = freq3;
         this.volRatio3 = vol3;
         this.timeRatio3 = ms3;
-        this.partA.src = music.createSoundEffect(wave, 333, 333, 222, 222, 999, fx, shape)
+        this.partC = new soundExpression.Sound;
+        this.partC.src = music.createSoundEffect(wave, 333, 333, 666, 666, 999, fx, shape);
         this.usingC = true
-
     }
 
 
-    // methods... 
-    utterUsing(freq: number, vol: number, ms: number) {
+    performUsing(freq: number, vol: number, ms: number) {
         let loudness = vol * 4 // map from [0...255] into range [0...1023]
         // adjust PartA duration, frequencies and volumes 
         this.partA.src = this.insert(this.partA.src, durationPos, this.formatNumber(ms * this.timeRatio1, 4));
@@ -135,7 +169,7 @@ function emitTweet(pitch: number, ms: number) {
         InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
 }
 */
-let tweet = new Utterance(VoxType.TWEET, 0.8, 120,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Logarithmic,1.00, 200, 1.0);
+let tweet = new Utterance(UtteranceType.TWEET, 0.8, 120,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Logarithmic,1.00, 200, 1.0);
 /*
 LAUGH	70	100	
 SAW	NONE	LOG	100	250	10	
@@ -151,7 +185,7 @@ function emitLaugh(pitch: number, ms: number){
 }
 
 */
-let laugh = new Utterance(VoxType.LAUGH, 0.70, 100,WaveShape.Sawtooth, SoundExpressionEffect.None, InterpolationCurve.Logarithmic,1.00, 250, 0.1)
+let laugh = new Utterance(UtteranceType.LAUGH, 0.70, 100,WaveShape.Sawtooth, SoundExpressionEffect.None, InterpolationCurve.Logarithmic,1.00, 250, 0.1)
     laugh.usePartB(WaveShape.Square, SoundExpressionEffect.None, InterpolationCurve.Linear, 0.7, 180, 0.9);
 
 /* SNORE -3508	25
@@ -167,7 +201,7 @@ function emitSnore(ms: number) {
         InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 }
 */
-let snore = new Utterance(VoxType.SNORE, 3508, 150,WaveShape.Noise, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear,715, 250, 0.50)
+let snore = new Utterance(UtteranceType.SNORE, 3508, 150,WaveShape.Noise, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear,715, 250, 0.50)
 snore.usePartB(WaveShape.Noise, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear,5008, 10, 0.50);
 /*
 DOO	300	200	
@@ -183,7 +217,7 @@ function emitDoo(pitch: number, ms: number) {
 }
 
 */
-let doo = new Utterance(VoxType.DOO, 3.00, 200,WaveShape.Sawtooth, SoundExpressionEffect.None, InterpolationCurve.Logarithmic,1.00, 220, 0.95)
+let doo = new Utterance(UtteranceType.DOO, 3.00, 200,WaveShape.Sawtooth, SoundExpressionEffect.None, InterpolationCurve.Logarithmic,1.00, 220, 0.95)
 doo.usePartB(WaveShape.Square, SoundExpressionEffect.None, InterpolationCurve.Linear,1.00, 180, 0.05);
 
 /*
@@ -191,7 +225,7 @@ QUERY	110	50
 SQU	NONE	LIN	100	250	70	
 SQU	NONE	CUR	150	50	20
 */
-let query = new Utterance(VoxType.QUERY, 1.30, 150,WaveShape.Square, SoundExpressionEffect.None, InterpolationCurve.Linear,1.00, 250, 0.7)
+let query = new Utterance(UtteranceType.QUERY, 1.30, 150,WaveShape.Square, SoundExpressionEffect.None, InterpolationCurve.Linear,1.00, 250, 0.7)
 query.usePartB(WaveShape.Square, SoundExpressionEffect.None, InterpolationCurve.Curve,1.50, 50, 0.2);
 
 
@@ -211,7 +245,7 @@ function emitUhOh(pitch: number, ms: number) {
 }
 
 */
-let uhOh = new Utterance(VoxType.UHOH, 1.30, 150,WaveShape.Sawtooth, SoundExpressionEffect.None, InterpolationCurve.Curve,1.40, 250, 0.2)
+let uhOh = new Utterance(UtteranceType.UHOH, 1.30, 150,WaveShape.Sawtooth, SoundExpressionEffect.None, InterpolationCurve.Curve,1.40, 250, 0.2)
 uhOh.usePartB(WaveShape.Sawtooth, SoundExpressionEffect.None, InterpolationCurve.Curve,1.40, 250, 0.2)
 uhOh.usePartC(WaveShape.Square, SoundExpressionEffect.None, InterpolationCurve.Linear,1.00, 180, 0.55);
 
@@ -221,7 +255,7 @@ TRI	NONE	CUR	100	250	60
 TRI	NONE	CUR	95	200	30	
 TRI	NONE	LIN	115	133	10
 */
-let moan = new Utterance(VoxType.MOAN, 1.30, 150,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,1.00, 250, 0.6)
+let moan = new Utterance(UtteranceType.MOAN, 1.30, 150,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,1.00, 250, 0.6)
 moan.usePartB(WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,0.95, 200, 0.3)
 moan.usePartB(WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Linear,1.15, 133, 0.1);
 
@@ -233,7 +267,7 @@ SQU	NONE	LIN	95	200	30
 SQU	NONE	LIN	110	250	10	
 SQU	NONE	LIN	66	90	60
 */
-let duh = new Utterance(VoxType.DUH, 1.30, 150,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,1.00, 250, 0.60)
+let duh = new Utterance(UtteranceType.DUH, 1.30, 150,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,1.00, 250, 0.60)
 duh.usePartB(WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,0.95, 200, 0.30)
 duh.usePartB( WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Linear, 1.15, 133, 0.10);
 
@@ -256,7 +290,7 @@ function emitWaah(pitch: number, ms: number) {
 }
 
 */
-let waah = new Utterance(VoxType.MOAN, 1.30, 150,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,1.00, 250, 0.60)
+let waah = new Utterance(UtteranceType.MOAN, 1.30, 150,WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,1.00, 250, 0.60)
 waah.usePartB(WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve,0.95, 200, 0.30)
 waah.usePartC(WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Linear,1.15, 133, 0.10);
 
@@ -280,7 +314,7 @@ function emitGrowl(pitch: number, ms: number) {
         InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 }
 */
-let growl = new Utterance(VoxType.MOAN, 1.30, 150, WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve, 1.00, 250, 0.60)
+let growl = new Utterance(UtteranceType.MOAN, 1.30, 150, WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve, 1.00, 250, 0.60)
 growl.usePartB( WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Curve, 0.95, 200, 0.30)
 growl.usePartC( WaveShape.Triangle, SoundExpressionEffect.None, InterpolationCurve.Linear, 1.15, 133, 0.10);
 
@@ -295,10 +329,10 @@ function dumDiDum(repeat: number, period: number) {
         span = randint(0.2 * ave, 1.8 * ave)
         if (span > 0.5 * ave) {
             // mostly "Dum"...
-            doo.utterUsing(randint(150, 300), 1.0, span)
+            doo.performUsing(randint(150, 300), 1.0, span)
         } else {
             // .. with occasional higher-pitched "Di"
-            doo.utterUsing(randint(350, 500), 1.0, 0.15 * ave)
+            doo.performUsing(randint(350, 500), 1.0, 0.15 * ave)
         }
         basic.pause(100)
     }
@@ -327,17 +361,17 @@ function giggle(repeat: number, period: number) {
     }
     quiet = true
 }
+*/
 function whistle(repeat: number, period: number) {
     quiet = false
     ave = period / repeat
     for (let index = 0; index < repeat; index++) {
         span2 = randint(0.4 * ave, 1.8 * ave)
-        emitTweet(randint(600, 1200), span2)
+        tweet.performUsing(randint(600, 1200), 200, span2)
         basic.pause(100)
     }
     quiet = true
 }
-
 
 
 input.onButtonPressed(Button.A, function () {
@@ -345,7 +379,7 @@ input.onButtonPressed(Button.A, function () {
         whistle(15, 3000)
     }
 })
-*/
+
 
 input.onButtonPressed(Button.B, function () {
     if (quiet) {
